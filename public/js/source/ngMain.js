abdbe3dc7b3e0a406dfb3cbe2447d3c11a73fa09
Steven A Moody <steve.moody2@gmail.com>
2015-11-20 13:11:41 -0700

    dynamic flex value works, can i animate?

--- public/js/source/ngMain.js
+++ public/js/source/ngMain.js
@@ -10,5 +10,31 @@ angular.module('ngSteve').controller('mainController', ['$mdSidenav', function($
     };
 
 
+    mainCtrl.grid = {
+        left  : "90",
+        mid   : "5",
+        right : "5"
+    };
+
+    mainCtrl.gridFocus = function(id){
+        if (id === 'left'){
+            mainCtrl.grid.left = '90';
+            mainCtrl.grid.mid = '5';
+            mainCtrl.grid.right = '5';
+        }
+        if (id === 'mid'){
+            mainCtrl.grid.left = '5';
+            mainCtrl.grid.mid = '90';
+            mainCtrl.grid.right = '5';
+        }
+        if (id === 'right'){
+            mainCtrl.grid.left = '5';
+            mainCtrl.grid.mid = '5';
+            mainCtrl.grid.right = '90';
+        }
+
+    };
+
+
     console.log('mainController loaded');
 }]);
