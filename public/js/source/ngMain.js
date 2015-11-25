5edaec4cb16f67894ce7b3a6a489ec5954a294e5
Steven A Moody <steve.moody2@gmail.com>
2015-11-24 20:02:10 -0700

    made colors prettier

--- public/js/source/ngMain.js
+++ public/js/source/ngMain.js
@@ -25,30 +25,31 @@ angular.module('ngSteve').controller('mainController', ['$mdSidenav', function($
         },
         focus : function(id){
             if (id === 'left'){
-                mainCtrl.grid.left.flex   = '90';
-                mainCtrl.grid.left.active = true;
-                mainCtrl.grid.mid.flex    = '5';
-                mainCtrl.grid.left.active = false;
-                mainCtrl.grid.right.flex  = '5';
-                mainCtrl.grid.left.active = false;
+                mainCtrl.grid.left.flex    = '90';
+                mainCtrl.grid.left.active  = true;
+                mainCtrl.grid.mid.flex     = '5';
+                mainCtrl.grid.mid.active   = false;
+                mainCtrl.grid.right.flex   = '5';
+                mainCtrl.grid.right.active = false;
 
             }
             if (id === 'mid'){
-                mainCtrl.grid.left.flex   = '5';
-                mainCtrl.grid.left.active = false;
-                mainCtrl.grid.mid.flex    = '90';
-                mainCtrl.grid.left.active = true;
-                mainCtrl.grid.right.flex  = '5';
-                mainCtrl.grid.left.active = false;
+                mainCtrl.grid.left.flex    = '5';
+                mainCtrl.grid.left.active  = false;
+                mainCtrl.grid.mid.flex     = '90';
+                mainCtrl.grid.mid.active   = true;
+                mainCtrl.grid.right.flex   = '5';
+                mainCtrl.grid.right.active = false;
             }
             if (id === 'right'){
-                mainCtrl.grid.left.flex   = '5';
-                mainCtrl.grid.left.active = false;
-                mainCtrl.grid.mid.flex    = '5';
-                mainCtrl.grid.left.active = false;
-                mainCtrl.grid.right.flex  = '90';
-                mainCtrl.grid.left.active = true;
+                mainCtrl.grid.left.flex    = '5';
+                mainCtrl.grid.left.active  = false;
+                mainCtrl.grid.mid.flex     = '5';
+                mainCtrl.grid.mid.active   = false;
+                mainCtrl.grid.right.flex   = '90';
+                mainCtrl.grid.right.active = true;
             }
+            console.log('mainCtrl.grid: ', mainCtrl.grid);
         }
     };
 
